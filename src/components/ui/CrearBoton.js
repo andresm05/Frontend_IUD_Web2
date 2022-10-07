18a43fc121f4bf09e9db3a8d75afab162561810b
Jaime Muñoz <jaime.munozq@udea.edu.co>
2022-10-06 23:52:54 -0500

    modificar boton crear

--- src/components/ui/CrearBoton.js
+++ src/components/ui/CrearBoton.js
@@ -1,10 +1,14 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
-export default function CrearBoton({ nombre, pag }) {
+export default function CrearBoton({ nombre }) {
   return (
     <div>
-      <Link type="button" className="btn btn-outline-success" to={pag}>
+      <button
+        type="button"
+        className="btn btn-primary"
+        data-bs-toggle="modal"
+        data-bs-target="#exampleModal"
+      >
         <svg
           xmlns="http://www.w3.org/2000/svg"
           width="16"
@@ -16,7 +20,7 @@ export default function CrearBoton({ nombre, pag }) {
           <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3v-3z" />
         </svg>
         Crear {nombre}
-      </Link>
+      </button>
     </div>
   );
 }
